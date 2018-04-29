dfa326a78ccd00ea0807bea4c2ea16c10f3b6edf
pamepeixinho <pameiupip@gmail.com>
2018-04-28 22:19:47 -0300

    use styled-components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+// import logo from './logo.svg';
+
+const Wrapper = styled.div`
+  background: blue;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <Wrapper>
+        <header>
+          <h1>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </Wrapper>
     );
   }
 }
